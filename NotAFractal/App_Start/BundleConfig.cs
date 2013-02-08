26da0f00e15f0173698892ff66470585b3c0f6a0
Xesyto <M8R-acpfdp@mailinator.com>
2013-02-08 16:40:26 +0100

    Importing base project

---
+++ NotAFractal/App_Start/BundleConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Optimization;
+
+namespace NotAFractal
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+        }
+    }
+}
---
+++ NotAFractal/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotAFractal.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
