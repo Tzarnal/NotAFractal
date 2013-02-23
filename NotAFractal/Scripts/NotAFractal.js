cc5d8d82df65d7b2d0be0b9bb6cd917a4155feb0
Xesyto <M8R-acpfdp@mailinator.com>
2013-02-23 20:31:12 +0100

    Sidebar now moves with the screen on the desktop

--- NotAFractal/Scripts/NotAFractal.js
+++ NotAFractal/Scripts/NotAFractal.js
@@ -48,6 +48,27 @@ toggle = function() {
     ;
 };
 
+$(function () {
+
+    var $sidebar = $("#sidebar"),
+        $window = $(window),
+        offset = $sidebar.offset(),
+        topPadding = 15;
+
+    $window.scroll(function () {
+        if ($window.scrollTop() > offset.top) {
+            $sidebar.stop().animate({
+                marginTop: $window.scrollTop() - offset.top + topPadding
+            });
+        } else {
+            $sidebar.stop().animate({
+                marginTop: 0
+            });
+        }
+    });
+
+});
+
 typeInfo = function() {
     var node = $(this).parent();
 
